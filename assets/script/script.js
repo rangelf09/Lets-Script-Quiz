f7e9d1eb1e4f519ef85c34e4c538d353665f98e2
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-07-24 00:00:17 -0500

    added the html layout and added css style sheet. Also added notes for the javascript.

---
+++ assets/script/script.js
@@ -0,0 +1,64 @@
+// 1. make a button in html
+// 2. create a timer
+    //  a. make a timer function in javascript
+// 3. make an event listener - put it on the button /correct it to the buttton
+// 4. present/show a question in html
+    // maybe: hide question in html before ready
+    // make an global variable arrary of all questions in javascript
+            // question is going to be an object
+// this is your question 
+//             var arrayOfQuestion = [
+//                 {
+//                     question: "What is function",
+//                     answer:[
+//                         "answer 1 is this",
+//                         "answer 2 is this",
+//                         "answer 3 is this"
+
+//                     ],
+//                     correctAnswer: 1
+//                 },
+//                 {
+//                     question: "What is function",
+//                     answer:[
+//                         "answer 1 is this",
+//                         "answer 2 is this",
+//                         "answer 3 is this"
+//                 }
+//             ]
+// //  code for target something in nested in nexted structre of objects and arrays
+// var clickedAnswer = document.querySelector('[answer-choice="1"]')
+// // one way to use
+// arrayOfQuestion[0].answer[1] 
+// // another way to use
+// if ( clickedAnswer.getAttribute("answer-choice") == arrayOfQuestion[0].correctAnswer){
+//     // give them points
+// }
+
+
+
+
+
+
+
+
+
+
+// // 1. when I answer a question
+// // 2. then I am presented with another question
+//     // 1a.making a button to click answer choices or make the answer choice a button
+//     // 2a. hide the previous or make the question container change textor remove the container element
+
+
+
+
+
+
+
+// // WHEN I answer a question incorrectly
+// // THEN  time is subtracted from the clock
+// // 1. if statement to check if the answer choice select is correct
+//     // if (wrong answer is clicked) {
+//         // do something with variable that keeps track of total time
+//         // time left timeleft = timeLeft - 1
+//     // }
